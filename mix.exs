e2c727cd5f784414d7a1edf808581feaafc0382f
Chris McDonald <963752+CMcDonald82@users.noreply.github.com>
2018-05-07 15:26:32 -0400

    add edeliver and distillery to mixfile

--- mix.exs
+++ mix.exs
@@ -38,7 +38,9 @@ defmodule PhoenixApiAuthStarter.Mixfile do
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:edeliver, "~> 1.5.0"},
+      {:distillery, "~> 1.5"}
     ]
   end
 
